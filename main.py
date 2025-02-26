59e7103dc81c8fc04f3f5182f52a2950c6797733
Bernardo Espinoza <bernardo.julian.espinoza@gmail.com>
2025-02-26 11:37:22 -0300

    Main code

---
+++ main.py
@@ -0,0 +1,18 @@
+import qrcode
+
+#Texto a codificar
+texto = "Pildoras de Programacion"
+
+qr = qrcode.QRCode(version=1, 
+                   box_size=10, 
+                   border=2)
+qr.add_data(texto)
+qr.make(fit=True)
+
+imagen_qr = qr.make_image(fill_color="blue", 
+                          back_color="white")
+imagen_qr.save("codigo_qr.png")
+
+
+
+
