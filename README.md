e80006787dd8d92f6a128dc260deac76a23671b7
Bernardo Espinoza <bernardo.julian.espinoza@gmail.com>
2025-02-26 12:07:25 -0300

    Last commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 44-PY-Qr-generator
 QR generator in python
+
+The pip dependencies are installed in the enviroment folder
+
+You dont need to run the command: pip3 install -r requirements.txt
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import qrcode
 
 #Texto a codificar
-texto = "Pildoras de Programacion"
+texto = "https://github.com/bje-dev"
 
-qr = qrcode.QRCode(version=1, 
-                   box_size=10, 
-                   border=2)
+qr = qrcode.QRCode(version=1, box_size=10, border=2)
 qr.add_data(texto)
 qr.make(fit=True)
 
-imagen_qr = qr.make_image(fill_color="blue", 
-                          back_color="white")
+imagen_qr = qr.make_image(fill_color="blue", back_color="white")
 imagen_qr.save("codigo_qr.png")
 
 
